0c42d6c402bc258e827c9574a36f0f245448faf7
Yu Feng <rainwoodman@gmail.com>
2016-09-27 23:56:22 -0700

    update rel imports in gmm.py

--- bananas/gmm.py
+++ bananas/gmm.py
@@ -14,11 +14,11 @@ import numpy as np
 from scipy import linalg
 from time import time
 
-from ..base import BaseEstimator
-from ..utils import check_random_state, check_array
-from ..utils.extmath import logsumexp
-from ..utils.validation import check_is_fitted
-from .. import cluster
+from sklearn.base import BaseEstimator
+from sklearn.utils import check_random_state, check_array
+from sklearn.utils.extmath import logsumexp
+from sklearn.utils.validation import check_is_fitted
+from sklearn import cluster
 
 from sklearn.externals.six.moves import zip
 
