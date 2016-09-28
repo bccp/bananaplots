2021f344506fdf08bae1db14b9a45f3c8f2cf0b9
Yu Feng <rainwoodman@gmail.com>
2016-09-28 02:54:42 -0700

    massive updates.
    Add vmin, vmax support via Feature objects
    
    Add crosshair
    
    Add colorscheme to surface to pick a colorscheme.
    
    Add linestyle and linewidth.

--- bananas/bananas.py
+++ bananas/bananas.py
@@ -24,66 +24,150 @@ class Bananas(object):
             -------
                 the surface object
         """
-        from matplotlib import cm
-
         if not surface in self.surfaces:
             self.surfaces[surface] = dict(
+                    colorscheme='r',
                     order=self._unique,
-                    label=str(surface),
-                    cmap=cm.jet,
+                    label=None,
+                    cmap=None,
+                    linewidth=1.0,
+                    linestyle='-',
+                    color=None,
+                    levels=[0.68, 0.95],
                     compiler_options={},
                     )
             self._unique = self._unique + 10
-        self.surfaces[surface].update(attrs)
 
+        self.surfaces[surface].update(attrs)
+        
         return surface
+
+    def get_surface_attr(self, surface, attr):
+        from matplotlib import cm
+
+        f = self.surfaces[surface]
+        if f[attr] is not None:
+            return f[attr]
+
+        if attr == 'label':
+            return str(surface)
+
+        if attr == 'color':
+            cmap = self.get_surface_attr(surface, 'cmap')
+            return cmap(0.3)
+
+        if attr == 'cmap':
+            color = f['colorscheme']
+            shorts = {'b' : 'blue',
+                         'r' : 'red',
+                         'g' : 'green',
+                         'y' : 'yellow',
+                         'm' : 'magenta',
+                         'k' : 'black'}
+            color = shorts.get(color, color)
+
+            return {'blue' : cm.Blues_r,
+                        'red'  : cm.Reds_r,
+                        'green'  : cm.Greens_r,
+                        'yellow'  : cm.Oranges_r,
+                        'magenta'  : cm.Purples_r,
+                        'black'  : cm.Greys_r,
+                        }[color]
 
     def set_feature(self, feature, **attrs):
         if not feature in self.features:
             self.features[feature] = dict(
                     order=self._unique,
-                    label=str(feature),
-                    range=(-1., 1.)
+                    label=None,
+                    range=None,
                     )
             self._unique = self._unique + 10
         self.features[feature].update(attrs)
 
-    def render(self, axes, f1, f2, **options):
-        axes.set_xlabel(self.features[f1]['label'])
-        axes.set_ylabel(self.features[f2]['label'])
+    def get_feature_attr(self, feature, attr):
+        if not feature in self.features:
+            self.set_feature(feature)
 
-        x = numpy.linspace(*self.features[f1]['range'], num=512)
-        y = numpy.linspace(*self.features[f2]['range'], num=512)
+        f = self.features[feature]
+        if f[attr] is not None:
+            return f[attr]
+
+        if attr == 'label':
+            return str(feature)
+
+        if attr == 'range':
+            mins = [s[feature].vmin for s in self.surfaces]
+            maxes = [s[feature].vmax for s in self.surfaces]
+            return (min(mins), max(maxes))
+
+    def render(self, axes, f1, f2, **options):
+        axes.set_xlabel(self.get_feature_attr(f1, 'label'))
+        axes.set_ylabel(self.get_feature_attr(f2, 'label'))
+
+        x = numpy.linspace(*self.get_feature_attr(f1,'range'), num=512)
+        y = numpy.linspace(*self.get_feature_attr(f2,'range'), num=512)
         X, Y = numpy.meshgrid(x, y)
 
+        filled = options.get('filled', True)
+        contour_labels = options.get('contour_labels', False)
+        crosshair = options.get('crosshair', False)
+
         for surface, attrs in _sorteditems(self.surfaces, 'order'):
-            compiler_options = attrs['compiler_options']
+            cmap = self.get_surface_attr(surface, 'cmap')
+            color = self.get_surface_attr(surface, 'color')
+            compiler_options = self.get_surface_attr(surface, 'compiler_options')
+            linestyle = self.get_surface_attr(surface, 'linestyle')
+            linewidth = self.get_surface_attr(surface, 'linewidth')
+            style = dict(linestyle=linestyle, linewidth=linewidth)
+
+            levels = self.get_surface_attr(surface, 'levels')
             m = surface.compile((f1, f2), **compiler_options)
             Z = m.confidence(X, Y)
-
-            if options.pop('filled', True):
+            if filled:
                 CS = axes.contourf(X, Y, Z,
-                        levels=[0, 0.68, 0.95],
+                        levels=[0] + levels,
                         vmin=0.0, vmax=1.0,
-                        cmap=attrs['cmap'], alpha=0.7)
+                        cmap=cmap, alpha=0.7)
 
             CS = axes.contour(X, Y, Z,
-                    levels=[0.68, 0.95],
+                    levels=levels,
                     vmin=0.0, vmax=2.0,
-                    cmap=attrs['cmap'])
+                    cmap=cmap, **style)
 
-            if options.pop('contour_labels', False):
+            if crosshair:
+                x = surface[f1].peak
+                y = surface[f2].peak
+                if x is not None and y is not None:
+                    axes.axvline(x, color=color, **style)
+                    axes.axhline(y, color=color, **style)
+
+            if contour_labels:
                 TXT = axes.clabel(CS)
 
     def render1d(self, axes, f1, **options):
-        axes.set_xlabel(self.features[f1]['label'])
-        x = numpy.linspace(*self.features[f1]['range'], num=512)
+        crosshair = options.get('crosshair', False)
+        range = self.get_feature_attr(f1, 'range')
+        axes.set_xlabel(self.get_feature_attr(f1, 'label'))
+        axes.set_xlim(range)
+        x = numpy.linspace(*range, num=512)
 
         for surface, attrs in _sorteditems(self.surfaces, 'order'):
-            compiler_options = attrs['compiler_options']
+            label = self.get_surface_attr(surface, 'label')
+            cmap = self.get_surface_attr(surface, 'cmap')
+            color = self.get_surface_attr(surface, 'color')
+            compiler_options = self.get_surface_attr(surface, 'compiler_options')
+            linestyle = self.get_surface_attr(surface, 'linestyle')
+            linewidth = self.get_surface_attr(surface, 'linewidth')
+            style = dict(linestyle=linestyle, linewidth=linewidth)
+
             m = surface.compile((f1, ), **compiler_options)
             Z = numpy.exp(m.lnprob(x))
-            axes.plot(x, Z, label=attrs['label'], color=attrs['cmap'](0.3))
+            axes.plot(x, Z, label=label, color=color, **style)
+
+            if crosshair:
+                c = surface[f1].peak
+                if c is not None:
+                    axes.axvline(c, color=color, **style)
 
     def rendernd(self, figure, features, gridspec=None, **options):
         from matplotlib.gridspec import GridSpec
@@ -93,7 +177,7 @@ class Bananas(object):
         if gridspec is None:
             gridspec = GridSpec(len(features), len(features), hspace=0, wspace=0)
 
-        corner = options.pop('corner', 'lower left')
+        corner = options.get('corner', 'lower left')
 
         axes = {}
 
@@ -107,13 +191,13 @@ class Bananas(object):
             axes[i, j] = ax
             visible = config[corner][0]
             if i == j:
-                self.render1d(ax, features[i])
+                self.render1d(ax, features[i], **options)
                 ax.locator_params(axis='y', nbins=5)
                 ax.yaxis.set_major_formatter(NullFormatter())
                 continue
 
             if visible(i, j):
-                self.render(ax, features[j], features[i])
+                self.render(ax, features[j], features[i], **options)
             else:
                 ax.set_axis_off()
 
@@ -141,13 +225,43 @@ class Bananas(object):
         proxies = []
         labels = []
         for surface, attrs in _sorteditems(self.surfaces, 'order'):
-            cmap = attrs['cmap']
-            proxies.append(mpatches.Patch(color=cmap(0.3)))
-            labels.append(attrs['label'])
+            label = self.get_surface_attr(surface, 'label')
+            color = self.get_surface_attr(surface, 'color')
+            proxies.append(mpatches.Patch(color=color))
+            labels.append(label)
         return proxies, labels
 
 class Surface(object):
     pass
+
+class Feature(object):
+    def __init__(self, data, vmin=None, vmax=None, peak=None):
+        if isinstance(data, Feature):
+            if vmin is None:
+                vmin = data.vmin
+            if vmax is None:
+                vmax = data.vmax
+            if peak is None:
+                peak = data.peak
+            data = data.data
+        else:
+            if vmin is None:
+                vmin = data.min()
+            if vmax is None:
+                vmax = data.max()
+
+        # only 1d feature is supported
+        assert len(numpy.shape(data)) == 1
+        self.data = data
+        self.vmin = vmin
+        self.vmax = vmax
+        self.peak = peak
+
+    def __add__(self, other):
+        return Feature(numpy.concatenate([self.data, other.data]),
+                    vmin=numpy.min(self.vmin, other.vmin),
+                    vmax=numpy.max(self.vmax, other.vmax),
+                    peak=self.peak if other.peak is None else other.peak)
 
 class GMMSurface(Surface):
     """
@@ -157,22 +271,22 @@ class GMMSurface(Surface):
     def __init__(self, **features):
 
         self.features = {}
-        for feature, value in features.items():
-            # only 1d feature is supported
-            assert len(numpy.shape(value)) == 1
-            self.features[feature] = value
+        for name, feature in features.items():
+            self.features[name] = Feature(feature)
 
     def __add__(self, other):
         if not isinstance(other, GMMSurface):
             return Surface.__add__(self, other)
 
         features = {}
-        for feature in self.features:
-            if not feature in other.features:
+        for name in self.features:
+            if not name in other.name:
                 continue
-            features[feature] = numpy.concatenate(
-                    [self.features[feature], other.features[feature]])
+            features[name] = self.features[name] + other.features[name]
         return GMMSurface(**features)
+
+    def __getitem__(self, name):
+        return self.features[name]
 
     def freeze(self, nc=1, nb=20, cov="full"):
         from itertools import product
@@ -210,12 +324,14 @@ class GMMSurface(Surface):
 
         """
         data = []
-        for feature in features:
-            # only 1d feature is supported
-            value = self.features[feature]
-            data.append(value.reshape(1, -1))
-
+        limits = []
+        for name in features:
+            # only 1d name is supported
+            feature = self.features[name]
+            data.append(feature.data.reshape(1, -1))
+            limits.append((feature.vmin, feature.vmax))
         data = numpy.concatenate(data, axis=0)
+        limits = numpy.array(limits)
 
         from sklearn import mixture
 
@@ -232,24 +348,29 @@ class GMMSurface(Surface):
         lnprob_cl = numpy.percentile(lnprob, 100 - confidence_levels * 100.)
         confidence_table = numpy.array([lnprob_cl, confidence_levels])
 
-        return Marginalized(model, confidence_table)
+        return Marginalized(model, confidence_table, limits)
 
 class Marginalized(object):
-    def __init__(self, model, confidence_table):
+    def __init__(self, model, confidence_table, limits):
         self.model = model
         self.confidence_table = confidence_table 
-
+        self.mins  = limits[:, 0]
+        self.maxes = limits[:, 1]
     def lnprob(self, *args):
         args = numpy.array(numpy.broadcast_arrays(*args), copy=True)
         shape = args[0].shape
         args = args.reshape(len(args), -1)
-        lnprob = self.model.score(args.T).reshape(shape)
+        mask = (args >= self.mins[:, None]).all(axis=0)
+        mask &= (args <= self.maxes[:, None]).all(axis=0)
+        lnprob = self.model.score(args.T)
+        lnprob[~mask] = - numpy.inf
+        lnprob = lnprob.reshape(shape)
         return lnprob
 
     def confidence(self, *args):
         x, y = self.confidence_table
         lnprob = self.lnprob(*args)
-        return numpy.interp(lnprob, x, y)
+        return numpy.interp(lnprob, x, y, left=1., right=0.)
 
 class FrozenSurface(Surface):
     def __init__(self, cache, metadata):
--- bananas/tests/test_bananaplots.py
+++ bananas/tests/test_bananaplots.py
@@ -16,31 +16,35 @@ def test_bananas():
     """ Overall integration test """
 
     numpy.random.seed(1234)
+    data = normal(size=(3, 2000))
     s1 = bananas.GMMSurface(
-            X=numpy.concatenate([normal(size=2000)]),
-            Y=numpy.concatenate([normal(size=2000)]),
-            Z=numpy.concatenate([normal(size=2000)]),
+            X=bananas.Feature(data[0], vmin=-5, vmax=5, peak=0.0),
+            Y=bananas.Feature(data[1], vmin=-5, vmax=5, peak=0.0),
+            Z=bananas.Feature(data[2], vmin=-5, vmax=5, peak=0.0),
         )
     s2 = bananas.GMMSurface(
-            X=1 + normal(size=4000),
-            Y=2 + normal(size=4000), 
-            Z=3 + normal(size=4000), 
+            X=bananas.Feature(1 + data[0], vmin=-5, vmax=5, peak=1.0),
+            Y=bananas.Feature(2 + data[1], vmin=-5, vmax=5, peak=2.0),
+            Z=bananas.Feature(3 + data[2], vmin=-5, vmax=5, peak=3.0),
             )
 
     #s3 = (s1 + s2).freeze()
     banana = bananas.Bananas()
 
-    banana.add_surface(s1, label="S1", cmap=cm.Reds_r)
-    banana.add_surface(s2, label="S2", cmap=cm.Blues_r)
+    banana.add_surface(s1, label="S1", colorscheme='r')
+    banana.add_surface(s2, label="S2", colorscheme='b')
     #banana.add_surface(s3, label="S1 + S2", cmap=cm.Greens_r, compiler_options=dict(nc=2))
 
-    banana.set_feature("X", range=(-6, 6))
-    banana.set_feature("Y", range=(-6, 6))
-    banana.set_feature("Z", range=(-6, 6))
+#    banana.set_feature("X", range=(-6, 6))
+#    banana.set_feature("Y", range=(-3, 4))
+#    banana.set_feature("Z", range=(-6, 4))
 
     fig = Figure()
 
-    axes = banana.rendernd(fig, ["X", "Y", "Z"], nc=1, nb=100)
+    axes = banana.rendernd(fig, ["X", "Y", "Z"], nc=1, nb=100,
+            contour_labels=True,
+            filled=True,
+            crosshair=True)
 
     handlers, labels = banana.get_legend_handlers_labels()
     axes[0, 2].legend(handlers, labels, loc='center')
@@ -99,7 +103,7 @@ def test_1d():
     #banana.add_surface(s3, label="S1 + S2", cmap=cm.Greens_r, compiler_options=dict(nc=2))
 
     banana.set_feature("X", range=(-6, 6))
-    banana.set_feature("Y", range=(-6, 6))
+    banana.set_feature("Y", range=(-3, 3))
     banana.set_feature("Z", range=(-6, 6))
 
     fig = Figure()
